46d2f529a9a19d842f1a604ef6a5e67702cda9cb
msula2 <msula2@uis.edu>
2024-02-16 18:54:38 +0500

    Create post request handler for /register

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = require('knex')({
+    client: 'pg',
+    version: '16.2',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'nutri',
+      password : 'dwryhs478',
+      database : 'nutridb'
+    }
+  });
+  
+
+app.post("/register", (req, res) => {
+   const {username, password} = req.body;
+   db('users')
+    .returning('*')
+    .insert({
+        username: username,
+        password: password
+    })
+    .then(response => {
+        res.json(response);
+    })
+    .catch(err => res.status(400).json(err));
+
+
+
+
+
+   
+});
+app.listen(3001, () => {
+    console.log("app is running on port 3001");
+})
